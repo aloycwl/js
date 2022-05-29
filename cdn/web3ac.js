f722a86bd81bd8d87fa3243cf2719bfbd6ae60cf
Aloysius Chan <aloycwl@gmail.com>
2022-05-29 22:12:21 +0800

    Update web3ac.js

--- cdn/web3ac.js
+++ cdn/web3ac.js
@@ -1,32 +1,61 @@
+u0 = '[]';
+ua = 'uint256';
 u1 = {
-  internalType: 'uint256',
+  internalType: ua,
   name: '',
-  type: 'uint256',
+  type: ua,
 };
 u2 = {
-  internalType: 'uint256[]',
+  internalType: ua + u0,
   name: '',
-  type: 'uint256[]',
+  type: ua + u0,
 };
+ub = 'address';
 u3 = {
-  internalType: 'address',
+  internalType: ub,
   name: '',
-  type: 'address',
+  type: ub,
 };
 u4 = {
-  internalType: 'address[]',
+  internalType: ub + u0,
   name: '',
-  type: 'address[]',
+  type: ub + u0,
+};
+uc = 'string';
+u5 = {
+  internalType: uc,
+  name: '',
+  type: uc,
+};
+u6 = {
+  internalType: uc + u0,
+  name: '',
+  type: uc + u0,
 };
 function waitTxt(a, b) {
-        $('#'+b).html(a > 0 ? 'Loading...' : '');
-      }
+  $('#' + b).html(a > 0 ? 'Loading...' : '');
+}
 async function LB() {
   return (await contract2.methods.balanceOf(acct).call()) / 1e18;
 }
+function formatURL(u) {
+  if (u.includes('ipfs://') && u.length > 9)
+    u = u.replace('ipfs://', 'https://ipfs.io/ipfs/');
+  else if (
+    /^(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/.test(
+      u
+    )
+  );
+  else u = '';
+  return u;
+}
 async function load(a, b) {
   if (typeof CSS != 'undefined')
-    $('head').append($('<meta name="viewport"content="width=device-width,initial-scale=1.0"><link rel="stylesheet">').attr('href', CSS));
+    $('head').append(
+      $(
+        '<meta name="viewport"content="width=device-width,initial-scale=1.0"><link rel="stylesheet">'
+      ).attr('href', CSS)
+    );
   if (typeof ethereum != 'undefined') {
     web3 = new Web3(ethereum);
     web3 = web3.eth;
@@ -51,13 +80,20 @@ async function load(a, b) {
   }
 }
 async function load2() {
-  contract2 = new web3a.Contract(
+  contract2 = new web3.Contract(
     [
       {
         inputs: [u3],
         name: 'balanceOf',
         outputs: [u1],
         stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        inputs: [u3, u1],
+        name: 'approve',
+        outputs: [],
+        stateMutability: 'nonpayable',
         type: 'function',
       },
     ],
