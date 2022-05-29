33c7605483a19d8418782e7b6f7029a5b727a9f9
Aloysius Chan <aloycwl@gmail.com>
2022-05-29 18:55:23 +0800

    Update web3ac.js

--- cdn/web3ac.js
+++ cdn/web3ac.js
@@ -23,7 +23,7 @@ async function LB() {
 }
 async function load(a, b) {
   if (typeof CSS != 'undefined')
-    $('head').append($('<link rel="stylesheet">').attr('href', CSS));
+    $('head').append($('<meta name="viewport"content="width=device-width,initial-scale=1.0"><link rel="stylesheet">').attr('href', CSS));
   if (typeof ethereum != 'undefined') {
     web3 = new Web3(ethereum);
     web3 = web3.eth;
